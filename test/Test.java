51afbbdf9be3e63e921409cea607480b768a3a88
L1fe1 <316772957@qq.com>
2018-11-02 15:02:18 +0800

    test

--- test/Test.java
+++ test/Test.java
@@ -7,7 +7,8 @@ public class Test {
     public static void main(String[] args) {
         Map map=new HashMap();
         map.put("a","aaa");
-        System.out.println(map.containsKey(null)); //输出true 如果这个map集合中有这个key就返回true
-        System.out.println(map.containsKey("b")); //输出false 没有就返回false
+        System.out.println(map.containsKey("a")); //输出true 如果这个map集合中有这个key就返回true
+        System.out.println(map.containsKey(null)); //输出false 没有就返回false
+        System.out.println(map.get(null));
     }
 }
